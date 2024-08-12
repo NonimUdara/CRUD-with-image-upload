21d739a51b8b384aeb3183bdda6511f7f195078b
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-08-12 16:32:30 +0530

    coding for image upload

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const User = require('../models/users');
+const multer = require('multer');
+
+//image upload
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "./uploads");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname)
+    },
+});
+
+var upload = multer({
+    storage: storage,
+}).single("image");
 
 router.get("/", (req, res) => {
     // res.send("Home page");
