5926b543ded1cc3cd45b973d3517fa2c3c1140cd
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-08-12 19:37:01 +0530

    create user update

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require('../models/users');
 const multer = require('multer');
+const fs = require("fs");
 
 //image upload
 var storage = multer.diskStorage({
@@ -90,6 +91,106 @@ router.get("/", async (req, res) => {
 
 router.get("/add", (req, res) => {
     res.render("add_users", {title: "Add users"});
+});
+
+//edit an user route
+// router.get('/edit/:id', (req, res) => {
+//     let id = req.params.id;
+//     User.findById(id, (err, user) => {
+//         if(err){
+//             res.redirect('/');
+//         } else {
+//             if(user == null){
+//                 res.redirect('/');
+//             } else {
+//                 res.render("edit_users", {
+//                     title: "Edit User",
+//                     user: user,
+//                 });
+//             }
+//         }
+//     });
+// });
+router.get('/edit/:id', async (req, res) => {
+    let id = req.params.id;
+    try {
+        const user = await User.findById(id);
+        if (user == null) {
+            res.redirect('/');
+        } else {
+            res.render("edit_users", {
+                title: "Edit User",
+                user: user,
+            });
+        }
+    } catch (err) {
+        res.redirect('/');
+    }
+});
+
+//update user route
+// router.post('/update/:id', upload, (req, res) => {
+//     let id = req.params.id;
+//     let new_image = '';
+
+//     if(req.file){
+//         new_image = req.file.filename;
+//         try{
+//             fs.unlinkSync('./uploads/' + req.body.old_image);
+//         } catch(err){
+//             console.log(err);
+//         }
+//     } else {
+//         new_image = req.body.old_image;
+//     }
+//     User.findByIdAndUpdate(id, {
+//         name: req.body.name,
+//         email: req.body.email,
+//         phone: req.body.phone,
+//         image: new_image,
+//     }, (err, result) => {
+//         if(err){
+//             res.json({ message: err.message, type:"danger" });
+//         } else {
+//             req.session.message = {
+//                 type: "success",
+//                 mnessage: "User Updated successfully!",
+//             };
+//             res.redirect('/')
+//         }
+//     });
+// });
+
+router.post('/update/:id', upload, async (req, res) => {
+    let id = req.params.id;
+    let new_image = '';
+
+    if(req.file){
+        new_image = req.file.filename;
+        try {
+            fs.unlinkSync('./uploads/' + req.body.old_image);
+        } catch(err) {
+            console.log(err);
+        }
+    } else {
+        new_image = req.body.old_image;
+    }
+
+    try {
+        await User.findByIdAndUpdate(id, {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            image: new_image,
+        });
+        req.session.message = {
+            type: "success",
+            message: "User Updated successfully!",
+        };
+        res.redirect('/');
+    } catch (err) {
+        res.json({ message: err.message, type: "danger" });
+    }
 });
 
 module.exports = router;
