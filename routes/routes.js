df0074b324ed127cbd8a4e2857347a4e3561a60f
Nonim Udara <87659260+NonimUdara@users.noreply.github.com>
2024-08-12 16:13:40 +0530

    setup front-end

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/users", (req, res) => {
-    res.send("All users");
+router.get("/", (req, res) => {
+    // res.send("Home page");
+    res.render("index", {title: "Home"});
+});
+
+router.get("/add", (req, res) => {
+    res.render("add_users", {title: "Add users"});
 });
 
 module.exports = router;
